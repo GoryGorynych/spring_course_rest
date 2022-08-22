d40815a6c2440b37032730ca63cacbae0324b7ad
Vladimit <gory.gorynych@gmail.com>
2022-08-23 00:06:51 +0300

    second commit

--- src/main/java/edu/gor/spring/rest/controller/MyRESTController.java
+++ src/main/java/edu/gor/spring/rest/controller/MyRESTController.java
@@ -1,11 +1,10 @@
 package edu.gor.spring.rest.controller;
 
 import edu.gor.spring.rest.entity.Employee;
+import edu.gor.spring.rest.exception_handling.NoSuchEmployeeException;
 import edu.gor.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,4 +20,41 @@ public class MyRESTController {
         List<Employee> allEmployees = employeeService.getAllEmployees();
         return allEmployees;
     }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+
+        if(employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with ID = " + id + " in Database");
+        }
+
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public  Employee addNewEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+
+        Employee employee = employeeService.getEmployee(id);
+        if (employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with id = " + id + " in database");
+        }
+        employeeService.deleteEmployee(id);
+
+        return "Employee with ID = " + id + " was deleted";
+    }
+
 }
